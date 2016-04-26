c5e4f4c2d29831151bdce1802787b9cf012c3e5e
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2016-04-27 01:30:49 +0200

    Revert "error: removed uneeded tests"
    This reverts commit 37592b57b31987519e59117d49bccd0207e67f15.

--- tests/check_error.c
+++ tests/check_error.c
@@ -20,6 +20,28 @@
 
 
 static void
+test_error_new(void **state)
+{
+    sb_error_t *error = sb_error_new(1, "bola %s");
+    assert_non_null(error);
+    assert_int_equal(error->code, 1);
+    assert_string_equal(error->msg, "bola %s");
+    sb_error_free(error);
+}
+
+
+static void
+test_error_new_printf(void **state)
+{
+    sb_error_t *error = sb_error_new_printf(2, "bola %s", "guda");
+    assert_non_null(error);
+    assert_int_equal(error->code, 2);
+    assert_string_equal(error->msg, "bola guda");
+    sb_error_free(error);
+}
+
+
+static void
 test_error_parser(void **state)
 {
     const char *a = "bola\nguda\nchunda\n";
@@ -83,6 +105,8 @@ int
 main(void)
 {
     const UnitTest tests[] = {
+        unit_test(test_error_new),
+        unit_test(test_error_new_printf),
         unit_test(test_error_parser),
         unit_test(test_error_parser_crlf),
     };
