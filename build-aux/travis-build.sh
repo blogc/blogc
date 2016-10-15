fd1e581ff0318f92ead6b334c8019ae44e98bdcf
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2016-10-15 04:08:35 +0200

    travis: trying to fix build

--- build-aux/travis-build.sh
+++ build-aux/travis-build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-set -ex
+set -x
 
 rm -rf build
 mkdir -p build
 
+git --no-pager log -1 --pretty=format:x . 2>&1
+git describe --abbrev=4 --match="$prefix*" HEAD
+git describe --abbrev=4 HEAD 2>/dev/null
+git rev-list "$vtag"..HEAD 2>/dev/null
+git --version >/dev/null 2>&1
 build-aux/git-version-gen .tarball-version
 
 pushd build > /dev/null
