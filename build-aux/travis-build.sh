6c534a7bb5898e13456c8f610f7001444cfe8d36
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2016-10-30 09:48:28 -0700

    build: travis: improve test running option/CFLAGS

--- build-aux/travis-build.sh
+++ build-aux/travis-build.sh
@@ -8,7 +8,7 @@ mkdir -p build
 pushd build > /dev/null
 
 ../configure \
-    CFLAGS="-Wall -g" \
+    CFLAGS="-Wall -g -O0" \
     --enable-ronn \
     --disable-silent-rules \
     --enable-tests \
