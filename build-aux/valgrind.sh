306e29bcfa7d963fb4841829227a1e56360bc75d
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2017-01-20 23:10:22 +0100

    build: fixed mixed-tab in valgrind script

--- build-aux/valgrind.sh
+++ build-aux/valgrind.sh
@@ -10,7 +10,7 @@ export TESTS_ENVIRONMENT="
 		--show-possibly-lost=no"
 
 if [[ "${1}" == *.sh ]]; then
-    exec "${@}"
+	exec "${@}"
 else
-    exec ${TESTS_ENVIRONMENT} "${@}"
+	exec ${TESTS_ENVIRONMENT} "${@}"
 fi
