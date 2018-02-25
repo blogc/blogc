f7ae4bffadd232a758dfe9dce7025899b6fe7bad
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2018-02-25 01:39:38 +0100

    Revert "blogc: make: mkdir may raise ENOENT"
    This reverts commit 0bc74a99b362a7ef58655bf3d4b29d5699c38c90.
    
    That change made no sense :x

--- src/blogc/main.c
+++ src/blogc/main.c
@@ -101,7 +101,7 @@ blogc_mkdir_recursive(const char *filename)
 #else
             (-1 == mkdir(fname, 0777)) &&
 #endif
-            (errno != EEXIST && errno != ENOENT))
+            (errno != EEXIST))
         {
             fprintf(stderr, "blogc: error: failed to create output "
                 "directory (%s): %s\n", fname, strerror(errno));
