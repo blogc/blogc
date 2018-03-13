bddf3b851e19b448d0bafb7a3b43d320731ce296
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2018-03-13 23:43:55 +0100

    travis: add clang-analyzer

---
+++ .travis/targets/clang-analyzer.sh
@@ -0,0 +1,38 @@
+build() {
+    default_configure \
+        --enable-silent-rules
+
+    local pn="$(grep PACKAGE_TARNAME config.h | cut -d\" -f2)"
+    local pv="$(grep PACKAGE_VERSION config.h | cut -d\" -f2)"
+    local p="${pn}-clang-analyzer-${pv}"
+
+    set +e
+    scan-build \
+        --use-cc="${CC}" \
+        -o reports \
+        make
+    #src/blogc/libblogc_la-debug.lo
+    local rv=$?
+    set -e
+
+    local num_reports=$(ls -1 reports | wc -l)
+    [[ ${num_reports} -eq 0 ]] && return ${rv}
+    [[ ${num_reports} -eq 1 ]]
+
+    local reports="reports/$(ls -1 reports)"
+
+    if [[ -d "${reports}" ]]; then
+        mv "${reports}" clang-analyzer
+        tar \
+            -cvJf "${p}.tar.xz" \
+            clang-analyzer
+        rv=1
+    fi
+
+    return ${rv}
+}
+
+deploy() {
+    FILES=( *.tar.xz )
+    [[ ${RV} -ne 0 ]] && [[ "x${CC}" = "xclang" ]]
+}
