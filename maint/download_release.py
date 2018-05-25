151b096f3d2859a9c77915477047c998cf2c1a95
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2018-05-25 22:11:28 +0200

    maint: download latest build by default

--- maint/download_release.py
+++ maint/download_release.py
@@ -4,27 +4,42 @@
 import os
 import re
 import requests
+import shutil
 import subprocess
 import sys
 
+re_version = re.compile(r'blogc-([^\'"]+)\.tar\.gz')
 re_distfile = re.compile(r'(blogc[^\'"]+)\.sha512')
 base_url = 'https://distfiles.rgm.io/blogc'
 cwd = os.path.dirname(os.path.abspath(__file__))
 
 
 def download_release(version):
-    release_url = '%s/blogc-%s' % (base_url, version)
-    r = requests.get('%s/' % release_url)
-    r.raise_for_status()
+    if version is None:
+        release_url = '%s/LATEST' % (base_url,)
+        r = requests.get(release_url)
+        r.raise_for_status()
+        match = re_version.search(r.content)
+        if match is None:
+            raise RuntimeError('Could not guess version')
+        version = match.group(1)
+    else:
+        release_url = '%s/blogc-%s' % (base_url, version)
+        r = requests.get(release_url)
+        r.raise_for_status()
+
+    dest_path = os.path.join(cwd, 'releases', version)
+    if os.path.exists(dest_path):
+        shutil.rmtree(dest_path)
+    os.makedirs(dest_path)
 
     for distfile in set(re_distfile.findall(r.content)):
         file_url = '%s/%s' % (release_url, distfile)
-        dest_path = os.path.join(cwd, 'releases', version)
-        subprocess.check_call(['wget', '-c', '-P', dest_path, file_url,
+        subprocess.check_call(['wget', '-P', dest_path, file_url,
                                '%s.sha512' % file_url])
         subprocess.check_call(['sha512sum', '-c', '%s.sha512' % distfile],
                               cwd=dest_path)
 
 
 if __name__ == '__main__':
-    download_release(sys.argv[1])
+    download_release(sys.argv[1] if len(sys.argv) > 1 else None)
