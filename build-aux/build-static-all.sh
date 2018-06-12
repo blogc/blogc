75e18f5357fd4f9811bd9f032901c2e7c169436c
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2018-06-13 00:01:25 +0200

    build: static-all: remove base directory

--- build-aux/build-static-all.sh
+++ build-aux/build-static-all.sh
@@ -2,12 +2,11 @@
 
 set -ex
 
-DESTDIR="${PN}-${TARGET}-amd64-${PV}"
-
 ${MAKE_CMD:-make} LDFLAGS="-all-static"
-${MAKE_CMD:-make} DESTDIR="${PWD}/root/${DESTDIR}" install
-install -m 644 ../LICENSE "root/${DESTDIR}/LICENSE"
+${MAKE_CMD:-make} DESTDIR="${PWD}/root" install
+install -m 644 ../LICENSE root/LICENSE
+echo "${PV}" > root/VERSION
 
 pushd root > /dev/null
-tar -cvJf "../${DESTDIR}.tar.xz" "${DESTDIR}"
+tar -cvJf "../${PN}-${TARGET}-amd64-${PV}.tar.xz" *
 popd > /dev/null
