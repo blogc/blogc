a6edfdee8beb53aef708c7d5c90c9aeada23d6bc
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2018-12-20 01:08:54 +0100

    removed blogc-github-lambda
    it will be provided in a separate repository soon

--- build-aux/build-static.sh
+++ build-aux/build-static.sh
@@ -4,17 +4,4 @@ set -ex
 
 ${MAKE_CMD:-make} LDFLAGS="-all-static" blogc
 
-rm -rf root
-mkdir -p root
-
-install -m 755 blogc root/blogc
-install -m 644 src/blogc-github-lambda/lambda_function.py root/lambda_function.py
-install -m 644 ../LICENSE root/LICENSE
-strip root/blogc
-
-pushd root > /dev/null
-zip "../blogc-github-lambda-${PV}.zip" *
-popd > /dev/null
-
-install -m 755 root/blogc "blogc-static-amd64-${PV}"
-xz -z "blogc-static-amd64-${PV}"
+xz -zc blogc > "blogc-static-amd64-${PV}.xz"
