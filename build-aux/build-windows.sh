94364642e1735e697c8506423f9a60fb055e87af
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2018-12-22 02:49:51 +0100

    travis

--- build-aux/build-windows.sh
+++ build-aux/build-windows.sh
@@ -4,12 +4,12 @@ set -ex
 
 DESTDIR="${PN}-${TARGET}-${PV}"
 
-${MAKE_CMD:-make} blogc.exe
+${MAKE_CMD:-make} blogc.exe blogc-runserver.exe
 
 rm -rf "${DESTDIR}"
 mkdir -p "${DESTDIR}"
 
-cp .libs/blogc.exe "${DESTDIR}/"
+cp .libs/blogc.exe .libs/blogc-runserver.exe "${DESTDIR}/"
 cp ../LICENSE "${DESTDIR}/"
 cp ../README.md "${DESTDIR}/"
 
